05ddc1735eaca76733c183b5e9c05d1a3fee2711
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 22:01:44 +0530

    Welcome message for Hashtable program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hashtable Program");
+        }
+    }
+}
