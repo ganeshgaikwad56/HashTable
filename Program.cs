78baf2ee532c2bc7f34f8c2bcf53f75c497e7da8
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 22:08:24 +0530

    Ganesh Adding program file for UC1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace HashTableProgram
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hashtable Program");
+            //Console.WriteLine("Welcome to Hashtable Program");
+            Console.WriteLine("Welcome to the Hashtable programs");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+
+            Console.WriteLine(hash.GetV("5"));
+            Console.WriteLine(hash.GetV("0"));
+            Console.ReadLine();
         }
     }
 }
